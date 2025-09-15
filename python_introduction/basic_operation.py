ead9c028a8a419bc64149622603e24a9319da43b
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-09-15 22:54:15 +0100

    first problem

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+result1 = number1 + number2
+
+result2 = number1 - number2
+
+result3 = number1 * number1
+
+print(f'Addition of {number1} and {number2} is {result1}')
+print(f'Subtraction of {number1} and {number2} is {result2}')
+print(f'Multiplication of {number1} and {number2} is {result3}')
+
+
