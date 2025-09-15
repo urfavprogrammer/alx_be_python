72a8a3883edb6cf839cd4a27560876f3ce74a841
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-09-15 23:07:56 +0100

    adding the simple interest file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,16 @@
+# (I = P * R * T),
+# Create a file named simple_interest.py.
+# Define three variables in this file:
+# principal with a value of 1000 (representing $1000),
+# rate with a value of 0.05 (representing 5% annual interest rate),
+# time with a value of 3 (representing 3 years).
+# Calculate the simple interest using the formula provided and store the result in a variable named interest.
+# Print the calculated interest in a format: The simple interest is: [interest].
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f'he simple interest is: {interest}.')
