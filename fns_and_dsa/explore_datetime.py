e771e9f156e3200974d7c4dbba6a6e70e97d2afc
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-09-29 04:39:57 -0700

    Familiarizing myself with Python's datetime module by writing a script that performs specified operations with dates and times.

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+
+current_date = datetime.now()
+
+formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
+# print("Formatted datetime:", formatted_datetime)
+
+# future_time =  current_date + timedelta(days=7, hours=5)
+# past_time = current_date - timedelta(days=3, hours=2)
+
+def display_current_datetime():
+    current_date = formatted_datetime
+    print("Current date and time:", current_date)
+
+def calculate_future_time(days):
+    future_time = current_date + timedelta(days=days)
+    print("Future date:", future_time.strftime("%Y-%m-%d"))
+    return future_time
+
+display_current_datetime()
+days = int(input("Enter the number of days to add to the current date: "))
+
+calculate_future_time(days)
+
+
+
+# print("Past time (3 days ago):", past_time)
+# print("Future time (7 days later):", future_time)
