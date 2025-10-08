1b881566c3943a2c12d3ec1025e212649a5b4d12
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-10-08 08:54:26 -0700

    renaming error messages to suit checker 2

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator):
 		denom = float(denominator)
 		try:
 			result = num / denom
-			return result
+			return f"The result of the division is {result}"
 		except ZeroDivisionError:
 			return "Error: Cannot divide by zero."
 	except ValueError:
