8523a2c55c2d00e43ec5a13dd143c428d363899d
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-10-08 08:23:22 -0700

    suiting naming to pass checker

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,4 +14,6 @@ class BankAccount:
 			return False
 
 	def display_balance(self):
-		print(f"Current balance: ${self.account_balance:.2f}")
+		print(f"Current Balance: ${self.account_balance:.2f}")
+		
+       
