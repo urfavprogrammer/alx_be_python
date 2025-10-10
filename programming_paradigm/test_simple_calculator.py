e0a71fb7b3dec316791bd8cc669db40d4503861e
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-10-10 00:53:50 -0700

    solving errors 5

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,6 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
 		self.calc = SimpleCalculator()
 
 	def test_addition(self):
+		self.assertEqual(self.calc.add)
 		self.assertEqual(self.calc.add(2, 3), 5)
 		self.assertEqual(self.calc.add(-1, 1), 0)
 		self.assertEqual(self.calc.add(0, 0), 0)
