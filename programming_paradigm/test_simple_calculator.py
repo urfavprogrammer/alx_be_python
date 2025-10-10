c6218e18364aa0890798364e0d045281de935cab
Kenechukwu Ezebube <mickeeun@gmail.com>
2025-10-10 00:27:58 -0700

    renaming variable names to align with checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,11 +6,11 @@ class TestSimpleCalculator(unittest.TestCase):
 	def setUp(self):
 		self.calc = SimpleCalculator()
 
-	def test_add(self):
+	def test_addition(self):
 		self.assertEqual(self.calc.add(2, 3), 5)
 		self.assertEqual(self.calc.add(-1, 1), 0)
 
-	def test_subtract(self):
+	def test_subtraction(self):
 		self.assertEqual(self.calc.subtract(5, 3), 2)
 		self.assertEqual(self.calc.subtract(0, 5), -5)
 
